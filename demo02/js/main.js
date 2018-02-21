352a2fa45a3edaf8f0025c071d4976e46ca26433
DESKTOP-52Q97OD\rodney <rodney.addo@gmail.com>
2018-02-21 10:36:22 +0200

    first commit

---
+++ demo02/js/main.js
@@ -0,0 +1,16 @@
+
+// const ages = [10,12,25,56,60,68,23,14];
+
+// const old = ages.filter(age => age >= 60);
+// const old.map()
+
+// console.table(old);
+console.log('testing');
+
+
+
+const add = (numb1, numb2) => { 
+    console.log(numb1 + numb2);
+}
+
+add(4,6);
